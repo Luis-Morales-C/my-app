a020626b156770e8f014a6ad277b3b42596c7986
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-05-02 18:16:36 -0500

    Avance

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginDTO } from '../dto/login-dto';
+import { RegistroComponent } from '../registro/registro.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginDTO: LoginDTO;
+  registroComponent: RegistroComponent;
 
+  constructor() {
+    this.loginDTO=new LoginDTO;
+    this.registroComponent=new RegistroComponent;
+  }
+  public iniciarSesion() {
+    const email = this.loginDTO.email;
+    const password = this.loginDTO.password;
+
+    const listaRegistros = this.registroComponent.listaRegistros;
+
+    const usuarioEncontrado = listaRegistros.find(registro => registro.email === email && registro.password === password);
+    
+    if (usuarioEncontrado) {
+      console.log('Inicio de sesión exitoso');
+    } else {
+      console.log('Credenciales incorrectas');
+    }
+  }
 }
+
--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RegistroClienteDTO } from '../dto/registro-cliente-dto';
 import { FormsModule } from '@angular/forms';
+import { RegistroService } from '../registro/registroService';
+
 @Component({
 selector: 'app-registro',
 standalone: true,
@@ -8,15 +10,45 @@ imports: [FormsModule],
 templateUrl: './registro.component.html',
 styleUrl: './registro.component.css'
 })
-export class RegistroComponent {
-
+export class RegistroComponent implements OnInit{
+  ciudades: string[];
+  archivos!:FileList;
   registroClienteDTO: RegistroClienteDTO;
+  listaRegistros: RegistroClienteDTO[];
+  registroService: RegistroService;
 
   constructor() {
   this.registroClienteDTO = new RegistroClienteDTO();
+  this.listaRegistros=new Array;
+  this.ciudades = [];
+  this.cargarCiudades();
+  this.registroService=new RegistroService;
   }
 
+
   public registrar() {
+    if (this.registroClienteDTO.fotoPerfil != "") {
     console.log(this.registroClienteDTO);
+    this.registroService.agregarRegistro(this.registroClienteDTO);
+    } else {
+    console.log("Debe cargar una foto");
     }
+    }
+
+  public sonIguales(): boolean {
+    return this.registroClienteDTO.password == this.registroClienteDTO.confirmarPassword;
+        }
+
+  private cargarCiudades() {
+    this.ciudades = ["Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena"];
+    }   
+  
+  public onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.archivos = event.target.files;
+      this.registroClienteDTO.fotoPerfil = this.archivos[0].name;
+    }
+  }
+  ngOnInit(): void {
+  }
 }
