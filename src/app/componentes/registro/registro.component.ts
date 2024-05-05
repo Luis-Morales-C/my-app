ac0af08707d894467407226e320462b1767c34a6
CamiloAst <juanc.astudilloo@uqvirtual.edu.co>
2024-05-05 18:38:17 -0500

    Configuracion entorno

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -2,47 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { RegistroClienteDTO } from '../../dto/registro-cliente-dto';
 import { FormsModule } from '@angular/forms';
 import { RegistroService } from '../../servicios/registroService';
+import { CommonModule } from '@angular/common';
 
 @Component({
-selector: 'app-registro',
-standalone: true,
-imports: [FormsModule],
-templateUrl: './registro.component.html',
-styleUrl: './registro.component.css'
+  selector: 'app-registro',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
 })
-export class RegistroComponent implements OnInit{
+export class RegistroComponent implements OnInit {
   ciudades: string[];
-  archivos!:FileList;
+  archivos!: FileList;
   registroClienteDTO: RegistroClienteDTO;
   listaRegistros: RegistroClienteDTO[];
   registroService: RegistroService;
 
   constructor() {
-  this.registroClienteDTO = new RegistroClienteDTO();
-  this.listaRegistros=new Array;
-  this.ciudades = [];
-  this.cargarCiudades();
-  this.registroService=new RegistroService;
+    this.registroClienteDTO = new RegistroClienteDTO();
+    this.listaRegistros = new Array;
+    this.ciudades = [];
+    this.cargarCiudades();
+    this.registroService = new RegistroService;
   }
 
 
   public registrar() {
     if (this.registroClienteDTO.fotoPerfil != "") {
-    console.log(this.registroClienteDTO);
-    this.registroService.agregarRegistro(this.registroClienteDTO);
+      console.log(this.registroClienteDTO);
+      this.registroService.agregarRegistro(this.registroClienteDTO);
     } else {
-    console.log("Debe cargar una foto");
+      console.log("Debe cargar una foto");
     }
-    }
+  }
 
   public sonIguales(): boolean {
     return this.registroClienteDTO.password == this.registroClienteDTO.confirmarPassword;
-        }
+  }
 
   private cargarCiudades() {
     this.ciudades = ["Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena"];
-    }   
-  
+  }
+
   public onFileChange(event: any) {
     if (event.target.files.length > 0) {
       this.archivos = event.target.files;
