c310cb547561d538b9999bead49a00b066ea7b03
CamiloAst <juanc.astudilloo@uqvirtual.edu.co>
2024-05-18 16:05:19 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,22 +5,21 @@ import { RegistroComponent } from './componentes/registro/registro.component';
 import { GestionNegociosComponent } from './componentes/gestion-negocios/gestion-negocios.component';
 import { CrearNegocioComponent } from './componentes/crear-negocio/crear-negocio.component';
 import { DetalleNegocioComponent } from './componentes/detalle-negocio/detalle-negocio.component';
-import { BusquedaComponent } from './componentes/busqueda/busqueda.component'; 
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
 import { LoginGuard } from './permiso.service';
 
 
 
 export const routes: Routes = [
-{ path: '', component: InicioComponent },
-{ path: 'login', component: LoginComponent },
-{ path: 'registro', component: RegistroComponent },
-{ path: "gestion-negocios", component: GestionNegociosComponent },
-{ path: "crear-negocio", component: CrearNegocioComponent },
-{ path: "detalle-negocio/:codigo", component: DetalleNegocioComponent },
-{ path: "busqueda/:texto", component: BusquedaComponent},
-{ path: "**", pathMatch: "full", redirectTo: "" },
-{ path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-{ path: 'registro', component: RegistroComponent, canActivate: [LoginGuard] },
-
+    { path: '', component: InicioComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent },
+    { path: "gestion-negocios", component: GestionNegociosComponent },
+    { path: "crear-negocio", component: CrearNegocioComponent },
+    { path: "detalle-negocio/:codigo", component: DetalleNegocioComponent },
+    { path: "busqueda/:texto", component: BusquedaComponent },
+    { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+    { path: 'registro', component: RegistroComponent, canActivate: [LoginGuard] },
+    { path: "**", pathMatch: "full", redirectTo: "" },
 
 ];
